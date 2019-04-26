cbfd35b86034c38d6a2b0a4a6a733f1df20ac4e7
imdeepmind <infinityatme@gmail.com>
2019-04-26 23:29:10 +0530

    added routng system

--- enviar/src/App.js
+++ enviar/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { Button } from "reactstrap";
+import ReallySmoothScroll from 'really-smooth-scroll';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+import Login from './app/router/login';
+import Home from './app/router/home';
+import E404 from './app/router/e404';
+
+ReallySmoothScroll.shim();
+const baseUrl = process.env.PUBLIC_URL;
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  let auth = localStorage.getItem('user');
+  return (
+    <Route {...rest} render={(props) => (
+      auth
+        ? <Component {...props} />
+        : <Redirect to='/login' />
+    )} />
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-    <Button color="primary">enviar</Button>
-      enviar
-    </div>
+    <Router basename={baseUrl}>
+      <div className="App">
+        <Switch>
+          <Route path={'/login'} exact component={Login} />
+          <PrivateRoute path={'/'} exact component={Home} />
+          <Route component={E404} />
+        </Switch>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ enviar/src/app/router/e404/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const e404 = (props) => {
+    return (
+        <h1>e404 Page</h1>
+    )
+}
+
+export default e404;
