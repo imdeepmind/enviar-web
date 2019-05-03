31b67b2c223fb52b2deb8845828b5c7d45c92aac
imdeepmind <infinityatme@gmail.com>
2019-05-03 22:39:58 +0530

    updated the avatar

--- enviar/src/app/components/avatar/index.js
+++ enviar/src/app/components/avatar/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import sample from '../../../assets/img/account.jpg';
+import React, { Fragment } from 'react';
 
 import { RESOURCE } from '../../../constants/endpoints'
 
 const Avatar = (props) => {
     const width = props.width ? props.width : "48px";
     return (
-        <img className="class-avatar border border-primary" style={{width:width}} src={props.source === undefined || props.source === '' ? sample : `${RESOURCE}/profile/${props.source}/small`} alt={props.title} />
+        <Fragment>
+            {props.source ? (
+                <img 
+                    className="class-avatar border border-primary" 
+                    style={{width:width}} 
+                    src={`${RESOURCE}/profile/${props.source}/small`} 
+                    alt={props.title}
+                />
+            ) : (
+                <i className="far fa-user-circle" style={{fontSize: "3em"}}></i>
+            )}
+        </Fragment>
     )
 }
 
