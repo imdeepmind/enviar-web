6a39cdb5690c09350085170bb1a9648ec2607eef
imdeepmind <infinityatme@gmail.com>
2019-05-03 23:33:01 +0530

    fixed issues in users page infinite scrolling

--- enviar/src/app/redux/users/reducer.js
+++ enviar/src/app/redux/users/reducer.js
@@ -4,11 +4,11 @@ import {
 
 const INIT_STATE = {
     users: [],
-    loading: false,
-    limit: 0,
-    page: 0,
-    pages: 0,
-    total: 0,
+    loading: true,
+    limit: 1,
+    page: 1,
+    pages: 1,
+    total: 1,
     error: null
 };
 
--- enviar/src/app/router/users/index.js
+++ enviar/src/app/router/users/index.js
@@ -5,49 +5,47 @@ import { connect } from 'react-redux';
 
 import TopNav from '../../container/topNav';
 import Ucard from './components/ucard';
-import More from './components/more';
 
 import { users } from '../../redux/actions';
-
-
 
 class Users extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            page: 1,
-            limit: 3,
-        }
     }
     componentDidMount(){
         this.dataListRender();
     }
+    componentDidUpdate(nextProps){
+        
+    }
     dataListRender = () => {
-        const data = {
-            page: this.state.page,
-            limit: this.state.limit
+        if (this.props.userReducer.loading){
+            const data = {
+                page:  1,
+                limit: 3
+            }
+            this.props.users(data, this.props.history);
         }
-        this.props.users(data, this.props.history);
+        
     }
     loadMore = () => {
-        this.setState({
-            page: this.state.page + 1,
-            limit: this.state.limit
-        }, () => this.dataListRender());
+        if (this.props.userReducer.loading === false){
+            const data = {
+                page: this.props.userReducer.page + 1,
+                limit: 3
+            }
+            this.props.users(data, this.props.history);
+        }
+        
     }
     render(){
-        console.log('props',this.props);
-        console.log('state', this.state);
-        console.log(this.props.userReducer.pages)
-        console.log(this.state.page)
-        console.log(this.props.userReducer.pages >= this.state.page);
-
         const items = this.props.userReducer.users.map(val => {
             return (
                 <Ucard 
                     key={val._id}
                     avatar={val.avatar} 
                     username={val.username} 
+                    name={val.name}
                     status={val.status}
                     isBlocked={val.isBlocked}
                     isFollowers={val.isFollowers}
@@ -64,7 +62,7 @@ class Users extends Component{
                         <InfiniteScroll
                             pageStart={1}
                             loadMore={this.loadMore}
-                            hasMore={this.props.userReducer.pages >= this.state.page}
+                            hasMore={this.props.userReducer.pages >= this.props.userReducer.page}
                             loader={<div className="loader" key={0}>Loading ...</div>}
                         >
                             {items}
