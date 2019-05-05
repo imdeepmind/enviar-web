7801b3107d24771ebed108411a9bccd8e455cbd2
imdeepmind <infinityatme@gmail.com>
2019-05-06 01:49:15 +0530

    added a 404 oage

--- enviar/src/app/router/e404/index.js
+++ enviar/src/app/router/e404/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Container } from 'reactstrap';
+
+import TopNav from '../../container/topNav';
+
 
 const e404 = (props) => {
     return (
-        <h1>e404 Page</h1>
+        <Fragment>
+            <TopNav />
+            <Container className="d-flex justify-content-center align-items-center flex-column text-center">
+                <h1 className="display-1"><i className="fas fa-skull-crossbones"></i></h1>
+                <h2>404 PAGE NOT FOUND</h2>
+                <p>I think you should learn typing properly</p>
+            </Container>
+        </Fragment>
     )
 }
 
