7ed562cf1551731a39a1f5fe44665b4fae446dd7
imdeepmind <infinityatme@gmail.com>
2019-05-20 23:12:02 +0530

    added system to update display picture

---
+++ enviar/src/app/router/edit/components/dpForm.js
@@ -0,0 +1,83 @@
+import React, { Fragment } from 'react'
+import { Field, reduxForm, formValueSelector } from 'redux-form'
+import { required, file } from 'redux-form-validators';
+import { connect } from 'react-redux'
+import { Form, Button, Label } from 'reactstrap';
+
+const adaptFileEventToValue = delegate => e => delegate(e.target.files[0]);
+
+const inputFileStyle = {
+  display: 'none'
+}
+
+const inputFileLabelStyle = {
+  width: '100%',
+  background: '#f7f7f9',
+  border: '0 solid #ced4da',
+  overflow: 'hidden'
+}
+
+const previewImageStyle = {
+  width: '100%'
+}
+
+const FileInput = ({ 
+    input: { value: omitValue, onChange, onBlur, ...inputProps }, 
+    meta: omitMeta, 
+    ...props 
+  }) => {
+    // console.log(omitMeta)
+    //TODO: Need to fix the touched issue
+    return (
+      <Fragment>
+        <input
+          onChange={adaptFileEventToValue(onChange)}
+          onBlur={adaptFileEventToValue(onBlur)}
+          type="file"
+          {...props.input}
+          {...props}
+        />
+        {
+            !omitMeta.valid && omitMeta.error &&
+            <span className="has-error text-danger">{omitMeta.error}</span>
+        }
+      </Fragment>
+    );
+  };
+
+let CreatePost = (props) => {
+    const { handleSubmit, image } = props;
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Label for="img-upload" style={inputFileLabelStyle}>
+            {image ?
+               <img src={URL.createObjectURL(image)} style={previewImageStyle} alt="preview" /> : 
+               (
+                <div className="d-flex justify-content-center align-items-center flex-column text-center p-3 pt-5 pb-5  "> 
+                  <i className="fas fa-cloud-upload-alt" style={{fontSize:"3em"}}></i>
+                  Select a display picture to upload
+                </div>
+               )}
+            </Label>
+            <Field name="avatar" id="img-upload" accept="image/*" style={inputFileStyle} component={FileInput} type="file" value={null} validate={[required(),file({ maxSize: '512KB' }), file({ minSize: '3KB' })]} />
+            <Button color="primary" className="w-100 mt-3" type="submit">Save</Button>
+        </Form>
+    )
+}
+
+
+CreatePost=  reduxForm({
+    form: 'CreatePost'
+})(CreatePost)
+
+const selector = formValueSelector('CreatePost');
+
+CreatePost = connect(
+  state => {
+    const image = selector(state, 'avatar');
+    return {image};
+  }
+)(CreatePost);
+
+export default CreatePost;
+
--- enviar/src/app/router/edit/index.js
+++ enviar/src/app/router/edit/index.js
@@ -1,16 +1,66 @@
 import React, { Component, Fragment } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { connect } from 'react-redux';
+import { BeatLoader } from 'react-spinners';
 
 import TopNav from '../../container/topNav';
 import FloatingActionButton from '../../container/floatingActionButton';
+import Avatar from '../../components/avatar';
+import DpForm from './components/dpForm';
+
+import { getMe, editDp } from '../../redux/actions';
+
+const loading = {
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '30px'
+}
 
 class Edit extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          modalDp: false
+        };
+      }
+    
+    toggleDpModal = () => {
+        this.setState(prevState => ({
+            modalDp: !prevState.modalDp
+        }));
+    }
+    componentDidMount(){
+        this.props.getMe();
+    }
+    onImageUpdate = avatar => {
+        console.log('Updating image...', avatar);
+        this.props.editDp(avatar);
+        this.toggleDpModal();
+    }
     render(){
+        console.log(this.props);
         return(
             <Fragment>
                 <TopNav history={this.props.history} />
                 <Container>
-                    <h1>Welcome to Edit page</h1>
+                    {this.props.meReducer.loading ? <BeatLoader key={0} css={loading} /> : 
+                        this.props.meReducer.error ? "Error" : 
+                        <Row>
+                            <Modal isOpen={this.state.modalDp} toggle={this.toggleDpModal}>
+                                <ModalHeader toggle={this.toggleDpModal}>Update your display picture</ModalHeader>
+                                <ModalBody>
+                                    <DpForm onSubmit={this.onImageUpdate} />
+                                </ModalBody>
+                            </Modal>
+
+                            <Col xs="12" md="6" className="text-center" onClick={this.toggleDpModal}>
+                                <Avatar source={this.props.meReducer.me.avatar} title={this.props.meReducer.me.name} width="120px" quality="medium"/>
+                            </Col>
+                            <Col xs="6">
+                                Abhishek
+                            </Col>
+                        </Row>
+                    }
                 </Container>
                 <FloatingActionButton history={this.props.history} />
             </Fragment>
@@ -18,4 +68,11 @@ class Edit extends Component{
     }
 }
 
-export default Edit;
+const mapStateToProps = (state) => state;
+  
+export default connect(
+    mapStateToProps,
+    {
+        getMe, editDp
+    }
+)(Edit);
