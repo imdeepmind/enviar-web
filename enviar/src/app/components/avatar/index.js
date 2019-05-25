691420f225a6ee6f02780605dc71df2efb9817ea
imdeepmind <infinityatme@gmail.com>
2019-05-25 21:02:22 +0530

    added fallback image in every place

--- enviar/src/app/components/avatar/index.js
+++ enviar/src/app/components/avatar/index.js
@@ -4,6 +4,10 @@ import { RESOURCE } from '../../../constants/endpoints'
 import UserBlack from '../../../assets/img/user_black.png';
 import UserWhite from '../../../assets/img/user_white.png';
 
+const imageErr = (event, color) => {
+    event.target.src = color === 'light' ? UserWhite : UserBlack;
+}
+
 const Avatar = (props) => {
     const width = props.width ? props.width : "48px";
     const style = {
@@ -20,6 +24,7 @@ const Avatar = (props) => {
                     style={style} 
                     src={`${RESOURCE}/profile/${props.source}/${medium}`} 
                     alt={props.title}
+                    onError={(e) => imageErr(e, props.border)}
                 />
             ) : (
                 <img 
@@ -27,6 +32,7 @@ const Avatar = (props) => {
                     style={style} 
                     src={props.border === 'light' ? UserWhite : UserBlack} 
                     alt={props.title}
+                    onError={(e) => imageErr(e, props.border)}
                 />
             )}
         </Fragment>
