e845a05c28719bd715e6fe385e1aa4b2bed638e4
imdeepmind <infinityatme@gmail.com>
2019-06-01 01:21:49 +0530

    updated the me page for desktop screen

--- enviar/src/app/router/me/components/hero.js
+++ enviar/src/app/router/me/components/hero.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Button, ButtonGroup } from 'reactstrap';
+import { Row, Col, ButtonGroup, Container } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 import Avatar from '../../../components/avatar';
@@ -14,33 +14,33 @@ class Hero extends Component{
         else styleGender['background'] = 'hotpink';
 
         return(
-            <Row className="bg-primary">
-                <Col xs="12">
-                    <button onClick={() => this.props.history.goBack()} className="border-0 text-white bg-transparent p-2 pt-4"><i style={{fontSize:"1.2rem"}} className="fas fa-chevron-circle-left"></i></button>
+            <div className="bg-primary w-100">
+                 <button onClick={() => this.props.history.goBack()} className="border-0 text-white bg-transparent p-2 pt-4"><i style={{fontSize:"1.2rem"}} className="fas fa-chevron-circle-left"></i></button>
                     <span className="text-light p-2 pt-4 float-right font-weight-bold">{this.props.username}</span>
-                </Col>
-                <Col xs="12" md="6" className="text-center p-3">
-                    <Avatar source={this.props.avatar} title={this.props.name} width="160px" border="light" quality="medium"/>
-                </Col>
-                <Col xs="12" md="6" className="text-center text-light">
-                    <h1 className="text-light">{this.props.name}</h1>
-                    <h4 className="text-light">{this.props.status}</h4>
-                    <ButtonGroup className="mb-2 mt-2">
-                        <Button color="primary">
-                            <Link to="/edit" className="text-light">
-                                <i className="fas fa-user-edit"></i><br />Edit
-                            </Link>
-                        </Button>
-                        <Button color="primary">
-                            <Link to="/settings/" className="text-light">
-                                <i className="fas fa-user-cog"></i><br />Settings
-                            </Link>
-                        </Button>
-                    </ButtonGroup>
-                </Col>
-                <Col xs="12" style={styleGender}></Col>
-            </Row>
-        )
+                    <Container>
+                        <Row>
+                            <Col xs="12" md="4" className="text-center p-3 p-md-5">
+                                <Avatar source={this.props.avatar} title={this.props.name} width="160px" border="light" quality="medium"/>
+                            </Col>
+                            <Col xs="12" md="8" className="text-center text-md-left text-light d-md-flex justify-content-center flex-column align-items-start">
+                                <h1 className="text-light">{this.props.name}</h1>
+                                <h4 className="text-light">{this.props.status}</h4>
+                                <ButtonGroup>
+                                    <Link to="/edit" className="text-light m-3 ml-md-0 text-center text-decoration-none">
+                                        <i className="fas fa-user-edit"></i><br />Edit
+                                    </Link>
+                                    <Link to="/settings/" className="text-light m-3 ml-md-0 text-center text-decoration-none">
+                                        <i className="fas fa-user-cog"></i><br />Settings
+                                    </Link>
+                                </ButtonGroup>
+                            </Col>
+                        </Row>
+                    </Container>
+                    <div style={styleGender}></div>
+                </div>
+            )
+            
+           
     }
 }
 
--- enviar/src/app/router/me/index.js
+++ enviar/src/app/router/me/index.js
@@ -78,7 +78,6 @@ class Me extends Component {
     render(){
         return (
             <Fragment>
-                <Container>
                     {this.props.meReducer.loading ? <BeatLoader key={0} css={loading} /> : (
                         !this.props.meReducer.error ? 
                         <Fragment>
@@ -90,80 +89,95 @@ class Me extends Component {
                                 status={this.props.meReducer.me.status}
                                 gender={this.props.meReducer.me.gender}
                             />
-                            <div className="d-flex justify-content-between align-items-center" style={tabStyle}>
-                                <Button className={classnames({ 'border-bottom border-primary': this.state.activeTab === '1' })} onClick={() => { this.toggle('1'); }}><i className="fas fa-book-open"></i></Button>
-                                <Button className={classnames({ 'border-bottom border-primary': this.state.activeTab === '2' })} onClick={() => { this.toggle('2'); }}><i className="fas fa-user-friends"></i></Button>
-                                <Button className={classnames({ 'border-bottom border-primary': this.state.activeTab === '3' })} onClick={() => { this.toggle('3'); }}><i className="fas fa-users"></i></Button>
-                                <Button className={classnames({ 'border-bottom border-primary': this.state.activeTab === '4' })} onClick={() => { this.toggle('4'); }}><i className="fas fa-user-slash"></i></Button>
-                            </div>
-                            <TabContent activeTab={this.state.activeTab}>
-                                <TabPane tabId="1">
-                                    <Detail 
-                                        location={this.buildAddress()} 
-                                        dob={this.props.meReducer.me.dob} 
-                                        bio={this.props.meReducer.me.bio}
-                                    />
-                                </TabPane>
-                                <TabPane tabId="2">
-                                    {this.props.meReducer.loadingSmall ? <BeatLoader key={0} css={loading} /> : 
-                                        this.props.meReducer.following.length > 0 ? 
-                                        this.props.meReducer.following.map(val => {
-                                            return (
-                                                <Ucard 
-                                                    key={val._id}
-                                                    avatar={val.avatar} 
-                                                    username={val.username} 
-                                                    name={val.name}
-                                                    status={val.status}
-                                                    followee={true}
-                                                    action={this.userAction}
-                                                />
-                                            )
-                                        }) : <div className="text-center mt-2">please follow someone to see them in this list</div>
-                                    }
-                                </TabPane>
-                                <TabPane tabId="3">
-                                    {this.props.meReducer.loadingSmall ? <BeatLoader key={0} css={loading} /> : 
-                                        this.props.meReducer.followers.length > 0 ? 
-                                        this.props.meReducer.followers.map(val => {
-                                            return (
-                                                <Ucard 
-                                                    key={val._id}
-                                                    avatar={val.avatar} 
-                                                    username={val.username} 
-                                                    name={val.name}
-                                                    status={val.status}
-                                                    followers={true}
-                                                    action={this.userAction}
-                                                />
-                                            )
-                                        }) : <div className="text-center mt-2">hey do want to know the secret to get more followers, well the answer is just to ask all your friends to join enviar</div>
-                                    }
-                                </TabPane>
-                                <TabPane tabId="4">
-                                    {this.props.meReducer.loadingSmall ? <BeatLoader key={0} css={loading} /> : 
-                                        this.props.meReducer.blocked.length > 0 ? 
-                                        this.props.meReducer.blocked.map(val => {
-                                            return (
-                                                <Ucard 
-                                                    key={val._id}
-                                                    avatar={val.avatar} 
-                                                    username={val.username} 
-                                                    name={val.name}
-                                                    status={val.status}
-                                                    blocked={true}
-                                                    action={this.userAction}
-                                                />
-                                            )
-                                        }) : <div className="text-center mt-2">i really love empty lists</div>
-                                    }
-                                </TabPane>
-                            </TabContent>
+                            <Container>
+                                <div className="d-flex justify-content-between align-items-center" style={tabStyle}>
+                                    <Button className={classnames({ 'no-focus border-bottom border-primary': this.state.activeTab === '1' })} onClick={() => { this.toggle('1'); }}>
+                                        <i className="fas fa-book-open"></i>
+                                        <span className="d-none d-md-inline ml-md-1">Details</span>
+                                    </Button>
+                                    <Button className={classnames({ 'no-focus border-bottom border-primary': this.state.activeTab === '2' })} onClick={() => { this.toggle('2'); }}>
+                                        <i className="fas fa-user-friends"></i>
+                                        <span className="d-none d-md-inline ml-md-1">Followers</span>
+                                    </Button>
+                                    <Button className={classnames({ 'no-focus border-bottom border-primary': this.state.activeTab === '3' })} onClick={() => { this.toggle('3'); }}>
+                                        <i className="fas fa-users"></i>
+                                        <span className="d-none d-md-inline ml-md-1">Following</span>
+                                    </Button>
+                                    <Button className={classnames({ 'no-focus border-bottom border-primary': this.state.activeTab === '4' })} onClick={() => { this.toggle('4'); }}>
+                                        <i className="fas fa-user-slash"></i>
+                                        <span className="d-none d-md-inline ml-md-1">Blocked Users</span>
+                                    </Button>
+                                </div>
+                                <TabContent activeTab={this.state.activeTab}>
+                                    <TabPane tabId="1">
+                                        <Detail 
+                                            location={this.buildAddress()} 
+                                            dob={this.props.meReducer.me.dob} 
+                                            bio={this.props.meReducer.me.bio}
+                                        />
+                                    </TabPane>
+                                    <TabPane tabId="3">
+                                        {this.props.meReducer.loadingSmall ? <BeatLoader key={0} css={loading} /> : 
+                                            this.props.meReducer.following.length > 0 ? 
+                                            this.props.meReducer.following.map(val => {
+                                                return (
+                                                    <Ucard 
+                                                        key={val._id}
+                                                        avatar={val.avatar} 
+                                                        username={val.username} 
+                                                        name={val.name}
+                                                        status={val.status}
+                                                        followee={true}
+                                                        action={this.userAction}
+                                                    />
+                                                )
+                                            }) : <div className="text-center mt-2">please follow someone to see them in this list</div>
+                                        }
+                                    </TabPane>
+                                    <TabPane tabId="2">
+                                        {this.props.meReducer.loadingSmall ? <BeatLoader key={0} css={loading} /> : 
+                                            this.props.meReducer.followers.length > 0 ? 
+                                            this.props.meReducer.followers.map(val => {
+                                                return (
+                                                    <Ucard 
+                                                        key={val._id}
+                                                        avatar={val.avatar} 
+                                                        username={val.username} 
+                                                        name={val.name}
+                                                        status={val.status}
+                                                        followers={true}
+                                                        action={this.userAction}
+                                                    />
+                                                )
+                                            }) : <div className="text-center mt-2">hey do want to know the secret to get more followers, well the answer is just to ask all your friends to join enviar</div>
+                                        }
+                                    </TabPane>
+                                    <TabPane tabId="4">
+                                        {this.props.meReducer.loadingSmall ? <BeatLoader key={0} css={loading} /> : 
+                                            this.props.meReducer.blocked.length > 0 ? 
+                                            this.props.meReducer.blocked.map(val => {
+                                                return (
+                                                    <Ucard 
+                                                        key={val._id}
+                                                        avatar={val.avatar} 
+                                                        username={val.username} 
+                                                        name={val.name}
+                                                        status={val.status}
+                                                        blocked={true}
+                                                        action={this.userAction}
+                                                    />
+                                                )
+                                            }) : <div className="text-center mt-2">i really love empty lists</div>
+                                        }
+                                    </TabPane>
+                                </TabContent>
+
+                            </Container>
+                           
                         </Fragment>
                         : <AError title={this.props.meReducer.error} />
                     )}
                     <FloatingActionButton history={this.props.history} />
-                </Container>
             </Fragment>
             
         )
